8f52e25f1490ed0243f28acee3d9d7425ab60fab
Ben <taihuynh@Bens-MacBook-Air.local>
2019-04-09 10:49:43 +1000

    register email, validation  and back button

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import {AuthenticationService} from '../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  signupForm: FormGroup;
+  errorMessage: string = '';
+  validation_messages = {
+    'email': [
+        { type: 'required', message: 'Email is required' },
+        { type: 'email', message: 'Please enter the valid email' },
+        { type: 'maxlength', message: 'The email only accept 20 characters before @gmail.com' },
+        { type: 'pattern', message: 'The email only accept gmail.com and more than 5 characters long' }
+      ],
+    'password': [
+        { type: 'required', message: 'Password is required' },
+        { type: 'minLength', message: 'Only accept password equal or more than 6 characters'},
+      ]
+    
+  };
+  constructor(
+    public fAuthService: AuthenticationService,
+    public router:Router
+  ) { }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      email: new FormControl('',[ Validators.required,Validators.email,Validators.pattern('^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$'),Validators.maxLength(30)]),
+      password: new FormControl('', [Validators.required,Validators.minLength(6)])
+    });
+  }
+
+  emailSignup(userValue){
+    this.fAuthService.emailRegister(userValue)
+    .then(res => {
+      this.fAuthService.emailLogin(userValue)
+      .then(res => {
+        this.router.navigate(['/tabs/home']); 
+        this.signupForm.reset();
+      }, error => this.errorMessage = error.message)
+    }, error => this.errorMessage = error.message)
   }
 
 }
